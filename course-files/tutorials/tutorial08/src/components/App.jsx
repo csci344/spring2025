f4501ae49d7b1f6df5c162b85c7375d35d043cd6
Sarah <svanwart@unca.edu>
2025-03-25 22:22:18 -0400

    Updated gh-pages with new site content

--- course-files/tutorials/tutorial08/src/components/App.jsx
+++ course-files/tutorials/tutorial08/src/components/App.jsx
@@ -1,35 +1,17 @@
 import React from "react";
+import NavBar from "./NavBar";
 
 // custom components:
-import NavBar from "./NavBar";
-import Profile from "./Profile";
-import Suggestions from "./Suggestions";
-import Stories from "./Stories";
-import Posts from "./Posts";
-
-export default function App({ username, token }) {
+export default function App() {
     return (
         <>
-            {/* Navbar (already implemented for you ) */}
-            <NavBar username={username} />
+            <NavBar />
 
-            {/* Main Panel */}
-            <main className="mt-[100px] md:max-w-[61vw] md:mr-[50px] px-6 md:pl-[5vw] lg:pl-[10vw]">
-                {/* Stories Panel */}
-                <Stories token={token} />
-
-                {/* Posts */}
-                <Posts token={token} />
+            <main className="min-h-screen max-w-[1000px] mt-24 mx-auto">
+                <p>Put your design system components in the space below...</p>
             </main>
 
-            {/* Right Panel */}
-            <aside className="fixed top-[100px] left-[63vw] w-70 hidden md:block max-w-[300px]">
-                {/* Profile Panel */}
-                <Profile token={token} />
-
-                {/* Suggestions Panel */}
-                <Suggestions token={token} />
-            </aside>
+            <footer className="p-5 bg-white">footer goes here</footer>
         </>
     );
 }
