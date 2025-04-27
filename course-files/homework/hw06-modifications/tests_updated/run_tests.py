438863ebb9bf6570d32846d036c39f9d42d46755
Sarah <svanwart@unca.edu>
2025-04-27 17:09:34 -0400

    Updated gh-pages with new site content

--- course-files/homework/hw06-modifications/tests_updated/run_tests.py
+++ course-files/homework/hw06-modifications/tests_updated/run_tests.py
@@ -5,31 +5,30 @@ utils.modify_system_path()
 import unittest
 
 # import the tests you want to run with the python run_tests.py command:
-from tests.test_bookmark_delete import TestBookmarkDetailEndpoint
-from tests.test_bookmark_list_post import TestBookmarkListEndpoint
-from tests.test_like_delete import TestLikePostDetailEndpoint
-from tests.test_like_post import TestLikePostListEndpoint
-from tests.test_profile import TestProfileEndpoint
-from tests.test_stories_list import TestStoryListEndpoint
+from tests_updated.test_bookmark_delete import TestBookmarkDetailEndpoint
+from tests_updated.test_bookmark_list_post import TestBookmarkListEndpoint
+from tests_updated.test_like_delete import TestLikePostDetailEndpoint
+from tests_updated.test_like_post import TestLikePostListEndpoint
+from tests_updated.test_profile import TestProfileEndpoint
+from tests_updated.test_stories_list import TestStoryListEndpoint
 
 # New tests:
-from tests.test_login import TestLoginEndpoint
-from tests.test_logout import TestLogoutEndpoint
-from tests.test_posts_get_patch_delete import TestPostDetailEndpoint
-from tests.test_posts_list_post import TestPostListEndpoint
-from tests.test_token import TestTokenEndpoint, TestRefreshTokenEndpoint
-
+from tests_updated.test_login import TestLoginEndpoint
+from tests_updated.test_logout import TestLogoutEndpoint
+from tests_updated.test_posts_get_patch_delete import TestPostDetailEndpoint
+from tests_updated.test_posts_list_post import TestPostListEndpoint
+from tests_updated.test_token import (
+    TestTokenEndpoint,
+    TestRefreshTokenEndpoint,
+)
 
 # Optional tests:
-# from tests.test_comments_delete import TestCommentDetailEndpoint
-# from tests.test_comments_post import TestCommentListEndpoint
-# from tests.test_followers_list import TestFollowerListEndpoint
-# from tests.test_following_delete import TestFollowingDetailEndpoint
-# from tests.test_following_list_post import TestFollowingListEndpoint
-# from tests.test_suggestions_list import TestSuggestionListEndpoint
-
-
-
+# from tests_updated.test_comments_delete import TestCommentDetailEndpoint
+# from tests_updated.test_comments_post import TestCommentListEndpoint
+# from tests_updated.test_followers_list import TestFollowerListEndpoint
+# from tests_updated.test_following_delete import TestFollowingDetailEndpoint
+# from tests_updated.test_following_list_post import TestFollowingListEndpoint
+# from tests_updated.test_suggestions_list import TestSuggestionListEndpoint
 
 
 if __name__ == "__main__":
