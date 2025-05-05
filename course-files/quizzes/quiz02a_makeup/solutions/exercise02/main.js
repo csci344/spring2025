b128433aace4bcce2eaabc9c68fbfab9bfcf9633
Sarah <svanwart@unca.edu>
2025-05-05 07:41:24 -0400

    Updated gh-pages with new site content

--- course-files/quizzes/quiz02a_makeup/solutions/exercise02/main.js
+++
@@ -1,9 +0,0 @@
-function addCat() {
-    const container = document.querySelector("#animals");
-    container.insertAdjacentHTML("beforeend", `<img src="cat.jpg" />`);
-}
-
-function addDog() {
-    const container = document.querySelector("#animals");
-    container.insertAdjacentHTML("beforeend", `<img src="dog.jpg" />`);
-}
